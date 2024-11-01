cd1846052bb94caeb1faae0af42c909b3808795a
Duke0503 <minhducgl0503@gmail.com>
2024-11-01 08:43:25 +0700

    Update

--- src/main/java/com/klearn/klearn_website/config/VNPayConfig.java
+++ src/main/java/com/klearn/klearn_website/config/VNPayConfig.java
@@ -14,7 +14,7 @@ import java.util.*;
 @Component
 public class VNPayConfig {
     public static String vnp_PayUrl = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
-    public static String vnp_Returnurl = "/vnpay-payment";
+    public static String vnp_Returnurl = "/payment";
     public static String vnp_TmnCode = "05QYIY20";
     public static String vnp_HashSecret = "SBZS777HP4KDA9CKDJVKBTR5RC7UY3QO";
     public static String vnp_apiUrl = "https://sandbox.vnpayment.vn/merchant_webapi/api/transaction";
